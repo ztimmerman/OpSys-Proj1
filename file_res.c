79287768dc971d06bf5a1b7678b6822208759cb0
Zachary Timmerman <zwt13@my.fsu.edu>
2018-02-12 20:36:14 -0500

    Resolved issues with fileExists implementations causing segfaults when a file simply didn't exist.

--- file_res.c
+++ file_res.c
@@ -13,27 +13,27 @@
  * VARIABLE RETURNED MUST BE FREED IF NOT ERRORCODE
  */
 
-char * fileExists(const char * directory, const char * ourFile) {
+int fileExists(const char * directory, const char * ourFile) {
  DIR *dir;
  struct dirent *ent;
- char * newDir = 0;
+ int counter = 0;
  if ((dir = opendir (directory)) != NULL) {
    /* print all the files and directories within directory */
    while ((ent = readdir (dir)) != NULL) {
 //      printf ("%s\n", ent->d_name);
- if (strcmp(ent->d_name,ourFile) == 0 && newDir == 0)	{
-   newDir = calloc(strlen(directory) + strlen(ourFile) + 2,1);
-   strcpy(newDir,directory);
-   strcat(newDir,"/");
-   strcat(newDir,ourFile);
- }
+    if (strcmp(ent->d_name,ourFile) == 0)	{
+      closedir (dir);
+      return counter;
+    }  else  {
+      counter++;
+    }
    }
    closedir (dir);
-   return newDir;
+   return -2;
  } else {
    /* could not open directory */
 //    perror ("");
-   return 0;
+   return -1;
  }
 }
 
@@ -56,6 +56,7 @@ char * pathRes(char * newPath, const char * m_PATH){
     const char* itr = m_PATH;      //pointer to different parts of m_PATH
     const char* delim;            //pointer to delimiter of ':'
     //printf("%s\n",itr);
+    int foundDir = -3;
     for (int i = 0; i <= colons_num; i++) {
       temp_str = calloc(255,sizeof(char));
       delim = strchr(itr,':');
@@ -72,12 +73,13 @@ char * pathRes(char * newPath, const char * m_PATH){
 
     for (int i = 0; i <= colons_num; i++)  {
       //printf("%s\n",path_arr[i]);
-      temp_str = fileExists(path_arr[i],newPath);
-      if (temp_str != 0 && true_path == 0)  {
+      foundDir = fileExists(path_arr[i],newPath);
+      if (foundDir > -1 && true_path == 0)  {
         //printf("%s\n",temp_str);
-        true_path = calloc(strlen(temp_str) + 1,1);
-        strcpy(true_path,temp_str);
-        free(temp_str);
+        true_path = calloc(strlen(path_arr[i]) + strlen(newPath) + 2,1);
+        strcpy(true_path,path_arr[i]);
+        strcat(true_path,"/");
+        strcat(true_path,newPath);
       }
     }
 
@@ -106,12 +108,26 @@ char * homeProcess(char * oldPath, const char * homePath)  {
   }
 }
 
+char * relativeProcess(char * curr_pwd, char * obj)  {
+  char * newObj;
+  int isValid = fileExists(curr_pwd,obj);
+  if (isValid >= 0) {
+    newObj = calloc(strlen(curr_pwd) + strlen(obj) + 2,1);
+    strcpy(newObj,curr_pwd);
+    strcat(newObj,"/");
+    strcat(newObj,obj);
+    return newObj;
+  } else  {
+      return 0;
+  }
+}
+
 int main()
 {
   char * paths = "/home/zacharytimmerman/bin:/home/zacharytimmerman/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin";
-  char * pwd = "~/opsys/OpSys-Proj1";
+  char * pwd = "/home/zacharytimmerman/opsys/OpSys-Proj1";
   char * z_home = "/home/zacharytimmerman";
-  char * newPath = "javac";
+  char * newPath = ".git";
   char * true_path;
 
 /*
@@ -119,10 +135,19 @@ int main()
   printf("%s\n",true_path);
   free(true_path);
 */
+
+/*
   true_path = homeProcess(pwd,z_home);
   printf("%s\n",true_path);
   free(true_path);
-
+*/
+  true_path = relativeProcess(pwd,newPath);
+  if (true_path != 0) {
+    printf("%s\n",true_path);
+    free(true_path);
+  } else  {
+    printf("File not found.\n");
+  }
 
   return 0;
 }
